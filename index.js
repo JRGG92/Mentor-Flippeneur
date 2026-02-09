b106cedb09a399ada1112040f56beaf6b1847ca0
Juan Guerrero <naujguerrero92@gmail.com>
2026-02-09 00:38:06 -0700

    mantener servidor express activo

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Mentor Flippeneur vivo"); 
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Mentor Flippeneur activo");
+});
+
+app.listen(PORT, () => {
+  console.log("Mentor Flippeneur escuchando en puerto " + PORT);
+});
